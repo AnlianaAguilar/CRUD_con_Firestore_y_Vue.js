0dbaa8bed3389c6cab4259be4b47c07e1ed80ad7
anlianaaguilar <anlianaaguilar@gmail.com>
2022-07-19 04:12:47 -0400

    componente navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,59 @@
+<template>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        to="/newUser"
+        text
+      >
+        <span class="mr-2">Nuevo Usuario</span>
+        <v-icon>mdi-account-multiple-plus</v-icon>
+      </v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    name: 'Navbar-component',
+    // props: {},
+    data: function(){
+        return {}
+    },
+    // computed: {},
+    //methods: {}
+    // watch: {},
+    // components: {},
+    // mixins: [],
+    // filters: {},
+    // -- Lifecycle Methods
+    // -- End Lifecycle Methods
+}
+</script>
+
+<style scoped>
+    
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,32 @@
 <template>
-  <div>
-    {{Usuarios}}
-  </div>
+  <v-container>
+    <h1>Usuarios</h1>
+    <v-row justify="center">
+      <v-col v-for="user in Usuarios" :key="user.id">
+        <v-card>
+          <v-card-title>Usuario</v-card-title>
+          <v-card-text>{{user.Nombre}}</v-card-text>
+          <v-card-text>{{user.email}}</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import { mapActions, mapState } from "vuex";
+export default {
+  name: "Home-view",
 
-import {mapActions, mapState} from 'vuex'
-  export default {
-    name: 'Home-view',
-
-    components: {
-
-    },
-    computed:{
-      ...mapState(['Usuarios'])
-    },
-    methods: {
-      ...mapActions(['fetchUsers'])
-    },
-    created(){
-      this.fetchUsers()
-    }
-  }
+  components: {},
+  computed: {
+    ...mapState(["Usuarios"]),
+  },
+  methods: {
+    ...mapActions(["fetchUsers"]),
+  },
+  created() {
+    this.fetchUsers();
+  },
+};
 </script>
