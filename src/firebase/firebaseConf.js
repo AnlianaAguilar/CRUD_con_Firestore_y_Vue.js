14100595b05da6c260e6cd5bf64b159387ece1ab
anlianaaguilar <anlianaaguilar@gmail.com>
2022-07-19 03:15:20 -0400

    integrando firebase

---
+++ src/firebase/firebaseConf.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAK6FSrkPMrodPcvYGJ-7LPM2R9_nNO2VU",
+    authDomain: "crud-con-firestore-bd855.firebaseapp.com",
+    projectId: "crud-con-firestore-bd855",
+    storageBucket: "crud-con-firestore-bd855.appspot.com",
+    messagingSenderId: "660022407202",
+    appId: "1:660022407202:web:e0f1c05c3c4be9fe961a7b"
+  };
