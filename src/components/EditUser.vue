7317d093b3e507ca427116efee234ab01a15619a
anlianaaguilar <anlianaaguilar@gmail.com>
2022-07-19 21:28:39 -0400

    se crea deleteUser y EditUser

--- src/components/EditUser.vue
+++ src/components/EditUser.vue
@@ -22,7 +22,8 @@
             ></v-text-field>
           </v-col>
           <v-col>
-            <v-btn @click="addUser">Agregar</v-btn>
+            <v-btn @click="EditUser">Editar</v-btn>
+            <v-btn @click="deleteUser">Eliminar</v-btn>
           </v-col>
         </v-row>
       </v-container>
@@ -32,10 +33,10 @@
 
 <script>
 import { db } from "@/firebase/firebase.js";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc, deleteDoc } from "firebase/firestore";
 export default {
   name: "EditUser-component",
-  props: ['id'], 
+  props: ["id"],
   data: function () {
     return {
       valid: true,
@@ -54,36 +55,45 @@ export default {
   },
   // computed: {},
   methods: {
-      
-     async fetchUserById(){
-         console.log(this.id)
-          let response = await getDoc(doc(db,'Usuarios', this.id))
-          console.log(response)
-          this.form = response.data() //se muestran las dato del usuario          
-      },
+    async fetchUserById() {
+      console.log(this.id);
+      let response = await getDoc(doc(db, "Usuarios", this.id));
+      console.log(response);
+      this.form = response.data(); //se muestran las dato del usuario
+    },
 
-
-    async addUser() {
+    async EditUser() {
       this.$refs.form.validate(); //validate cambia el estado de v-model="valid"
       if (!this.valid) return;
 
       try {
-    //     let response = await addDoc(collection(db, "Usuarios"), this.form);
-    //     console.log(response);
+        let user = this.form;
+        user.id = this.id;
+        await updateDoc(doc(db, "Usuarios", user.id), user);
         this.$router.push("/");
+
       } catch (error) {
         console.log(error);
       }
     },
+
+    deleteUser(){
+        try {
+            deleteDoc(doc(db, 'Usuarios', this.id))
+            this.$router.push("/");
+        } catch (error) {
+            console.log(error)
+        }
+    }
   },
   // watch: {},
   // components: {},
   // mixins: [],
   // filters: {},
   // -- Lifecycle Methods
-  created(){
-      this.fetchUserById()
-  }
+  created() {
+    this.fetchUserById();
+  },
   // -- End Lifecycle Methods
 };
 </script>
